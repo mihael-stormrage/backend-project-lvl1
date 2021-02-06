3fc3e8a699eb2bc948a75c1c12269de930838e22
Mihael Stormrage <mihan.msn@hotmail.com>
2021-02-07 06:25:26 +0700

    Fix code style

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export const greet = () => {
+const greet = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   return name;
 };
+
+export default greet;
