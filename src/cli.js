2984cba87d9f05c369207c99c7b5119b738533d5
Mihael Stormrage <mihan.msn@hotmail.com>
2021-02-09 04:45:15 +0700

    CLI: add ask assert & move welcome logic

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,17 @@
 import readlineSync from 'readline-sync';
 
 const greet = () => {
+  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   return name;
 };
 
-export default greet;
+const ask = (expected) => {
+  const answer = readlineSync.question('Your answer: ');
+  const isRight = answer === expected;
+  console.log(isRight ? 'Correct!' : `'${answer}' is wrong answer ;(. Correct answer was '${expected}'.`);
+  return isRight;
+};
+
+export { greet as default, ask };
