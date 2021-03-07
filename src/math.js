e74521299e3a01314e00da15984712f4c9f73a1b
Mihael Stormrage <mihan.msn@hotmail.com>
2021-03-08 03:40:32 +0700

    Add isPrime(n)

--- src/math.js
+++ src/math.js
@@ -4,4 +4,12 @@ export const gcd = (a, b) => (!b ? a : gcd(b, a % b));
 
 export const sequence = (start, step, n = 1) => start + (n - 1) * step;
 
+export const isPrime = (n) => {
+  if (n < 2) return false;
+  for (let i = 2; i <= Math.sqrt(n); i += 1) {
+    if (!(n % i)) return false;
+  }
+  return true;
+};
+
 export default getRandInt;
