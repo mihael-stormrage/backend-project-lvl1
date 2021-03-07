4ac1701a63a151b9aa685d3f0eed00198ace5d56
Mihael Stormrage <mihan.msn@hotmail.com>
2021-03-08 01:51:42 +0700

    Implement sequence element calculation

--- src/math.js
+++ src/math.js
@@ -2,4 +2,6 @@ const getRandInt = (min = 0, max = 100) => Math.floor(Math.random() * (max - min
 
 export const gcd = (a, b) => (!b ? a : gcd(b, a % b));
 
+export const sequence = (start, step, n = 1) => start + (n - 1) * step;
+
 export default getRandInt;
