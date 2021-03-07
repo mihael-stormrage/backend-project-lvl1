35c137d97837ded39b22126de4074cc876d66b3b
Mihael Stormrage <mihan.msn@hotmail.com>
2021-03-08 04:21:53 +0700

    Add "Try again!" message

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import greet from './cli.js';
 const makeGame = (brainGame) => {
   const name = greet();
   if (brainGame && brainGame()) console.log(`Congratulations, ${name}!`);
+  else console.log(`Let's try again, ${name}!`);
 };
 
 export default makeGame;
